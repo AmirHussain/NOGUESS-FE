972c34a0f37c7e1ccab74b400ddbbc8105e8f341
jahanzaibrashid <jahanzaib365@gmail.com>
2022-08-18 16:10:18 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
+//pages imports
 import Home from './Components/Home/home';
+import Lending from './pages/lending';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {routes} from "./routes"
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route path="*" exact component={Home}></Route>
+          <Route path={routes.lending} exact component={Lending}></Route>
         </Switch>
       </Router>
     </div>
